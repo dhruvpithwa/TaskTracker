0ff8e8aa80a4aafb7b16638e4cc971b54c830bc0
Dhruv Pithwa <dhruvpithwa1999@gmail.com>
2023-05-13 22:24:45 +0530

    Task Tracker using javascript, drag and drop api

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ function allowDrop(event) {
 function drop(event) {
     event.preventDefault();
     var fetchData = event.dataTransfer.getData("id");
-    
-    console.log(event.dataTransfer.getData("id"), fetchData);
     event.target.appendChild(document.getElementById(fetchData));
     event.dataTransfer.clearData("id");
 }
